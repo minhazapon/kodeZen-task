0bd9eb5da3527c5749c11fdb3522e7eceea12ea7
minhazapon <minhajulabedinapon@gmail.com>
2024-11-11 06:01:02 -0800

    added file

--- src/setting file/Setting.jsx
+++ src/setting file/Setting.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 
 const Setting = () => {
@@ -6,17 +5,11 @@ const Setting = () => {
         <div>
 
 
-           <div className=" kzui-block-page ">
+        <div className=" kzui-block-page ">
             
-            <Link to='/'>
-            <div>
-                <button className=" kzui-button1 kzui-the ">Go <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" height="20px" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                </svg>
-                </span> Design System Page</button>
+           <div>
+                <img src="https://cdn-icons-gif.flaticon.com/14447/14447642.gif" alt="" height="400px" width="400px" />
             </div>
-            </Link>
-            
             
         </div> 
              
